df719a098939266bf2b3faa8e4c9c718fd886e3d
kjwon15 <kjwonmail@gmail.com>
2014-10-18 22:44:13 +0900

    Add main javascript file

---
+++ deny_naver.js
@@ -0,0 +1,9 @@
+var crashNaver = function() {
+  var currentUrl = location.href;
+  var currentUrlEncoded = encodeURIComponent(currentUrl);
+  var href = 'naversearchapp://addshortcut?url=http%3A%2F%2F&icon=http%3A%2F%2F&title=naver&serviceCode=deny_naver&version=7';
+  var iframe = document.createElement('iframe');
+  iframe.src = href;
+  iframe.style.cssText = 'width: 1px; height: 0; border: 0;';
+  document.body.appendChild(iframe);
+};
